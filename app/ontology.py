6b9ea5d2912bea7f4f150eba79cd95585fd5a90b
Divrr <96827300+Divrr@users.noreply.github.com>
2024-10-06 19:05:45 +0800

    fixed out-of-order classes

--- app/ontology.py
+++ app/ontology.py
@@ -5,12 +5,12 @@ from owlready2 import get_ontology, sync_reasoner
 ontology_path = "ifixit_ontology.owl"
 onto = get_ontology(ontology_path).load()
 
-# Run the reasoner
-with onto:
-    sync_reasoner()
-
 print(f"Ontology loaded with {len(list(onto.Procedure.instances()))} procedures.")
 
-for procedure in onto.Procedure.instances():
-    if procedure.title and "macbook" in procedure.title.lower():
-        print(f"Procedure found: {procedure.title[0]}")
+# for cls in onto.classes():
+#     print(f"Class: {cls.name}")
+#     instances = list(cls.instances())
+#     if instances:
+#         print(f"  Number of instances: {len(instances)}")
+#         for instance in instances[:20]:
+#             print(f"    Instance: {instance.name}")
